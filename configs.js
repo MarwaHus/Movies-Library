310099df48ecc61838d0bbfbfbd10e2004001ac0
MARWA H <MARWA.HU18@GMAIL.COM>
2023-07-06 11:31:11 +0000

    lab15

--- configs.js
+++ configs.js
@@ -1,4 +1,9 @@
 module.exports = {
     PORT: process.env.PORT,
     DB_URL: process.env.DATABASE,
+    TRENDING_MOVIES:process.env.TRENDING_MOVIES,
+    SEARCH_MOVEIS:process.env.SEARCH_MOVEIS,
+    ID_MOVIES:process.env.ID_MOVIES,
+    API_KEY:process.env.API_KEY,
+    TOP:process.env.TOP
   };
